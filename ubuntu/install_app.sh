ea5039dd71e30d6d1386db8757be70c1535cd3a9
CodeJuan <CodeJuan@users.noreply.github.com>
2015-09-26 09:46:33 +0800

    set aliyun mirror

--- ubuntu/install_app.sh
+++ ubuntu/install_app.sh
@@ -16,27 +16,27 @@ else
 fi
 
 
-#echo install::update source...
-#if [ -f "/etc/apt/sources.list_backup" ];
-#then
-#	echo "install::source::File sources.list_backup exists."
-#	echo '#123' >> /etc/apt/sources.list_backup
-#else
-#	echo install::source::backup sources.list
-#	cp /etc/apt/sources.list /etc/apt/sources.list_backup
-#	echo '#aliyun' >> /etc/apt/sources.list
-#	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse' >> /etc/apt/sources.list
-#	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse' >> /etc/apt/sources.list
-#	apt-get update
-#fi
+echo install::update source...
+if [ -f "/etc/apt/sources.list_backup" ];
+then
+	echo "install::source::File sources.list_backup exists."
+	echo '#123' >> /etc/apt/sources.list_backup
+else
+	echo install::source::backup sources.list
+	cp /etc/apt/sources.list /etc/apt/sources.list_backup
+	echo '#aliyun' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+	echo 'deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse' >> /etc/apt/sources.list
+	apt-get update
+fi
 
 
 echo install::vim...
